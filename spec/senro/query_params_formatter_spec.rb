6cfa6bcb8511834de68720164e9beac493539f42
WalkerSumida <walkersumida@gmail.com>
2020-01-05 13:52:26 +0900

    test: add QueryParamsFormatter spec

---
+++ spec/senro/query_params_formatter_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+RSpec.describe Senro::QueryParamsFormatter do
+  describe '#sorting' do
+    context 'with +' do
+      it 'returns order clause containing ASC' do
+        expect(Senro::QueryParamsFormatter.sorting('+id')).to eq('id ASC')
+      end
+    end
+
+    context 'with -' do
+      it 'returns order clause containing DESC' do
+        expect(Senro::QueryParamsFormatter.sorting('-id')).to eq('id DESC')
+      end
+    end
+
+    context 'without + and -' do
+      it 'returns order clause containing ASC' do
+        expect(Senro::QueryParamsFormatter.sorting('id')).to eq('id ASC')
+      end
+    end
+
+    context 'with multiple attributes' do
+      it 'returns correct order clause' do
+        expect(
+          Senro::QueryParamsFormatter.sorting('+id,+name,created_at,-updated_at')
+        ).to eq('id ASC, name ASC, created_at ASC, updated_at DESC')
+      end
+    end
+  end
+end
--- spec/senro_spec.rb
+++ spec/senro_spec.rb
@@ -1,9 +1,7 @@
-RSpec.describe Senro do
-  it "has a version number" do
-    expect(Senro::VERSION).not_to be nil
-  end
+# frozen_string_literal: true
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Senro do
+  it 'has a version number' do
+    expect(Senro::VERSION).not_to be nil
   end
 end

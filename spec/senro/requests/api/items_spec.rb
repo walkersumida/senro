04b71709823c65b9a5acfd677fbc833b63ff0a80
WalkerSumida <walkersumida@gmail.com>
2020-01-25 13:08:04 +0900

    test: add ActionController::API spec

--- spec/senro/requests/api/items_spec.rb
+++ spec/senro/requests/api/items_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Items', type: :request do
-  describe 'GET /items?sort=id' do
+RSpec.describe 'Api::Items', type: :request do
+  describe 'GET /api/items?sort=id' do
     it 'returns a formatted sort param' do
-      get items_path, params: { sort: 'id' }
+      get api_items_path, params: { sort: 'id' }
 
       expect(response.request.env['action_controller.instance'].params['sort']).to eq('id ASC')
       expect(response.request.env['action_controller.instance'].params['original_sort']).to eq('id')
     end
   end
 
-  describe 'GET /items?sort=id,-name' do
+  describe 'GET /api/items?sort=id,-name' do
     it 'returns a formatted sort param' do
-      get items_path, params: { sort: 'id,-name' }
+      get api_items_path, params: { sort: 'id,-name' }
 
       expect(response.request.env['action_controller.instance'].params['sort']).to eq('id ASC, name DESC')
       expect(response.request.env['action_controller.instance'].params['original_sort']).to eq('id,-name')

74fa847939c14d305e61106eeac2269d1f524488
WalkerSumida <walkersumida@gmail.com>
2020-02-22 12:01:03 +0900

    feat: format query params

--- lib/senro/query_params_formatter.rb
+++ lib/senro/query_params_formatter.rb
@@ -22,5 +22,25 @@ module Senro
       end
       attributes.join(', ')
     end
+
+    # Format RESTful API's query params to SQL where clause.
+    #
+    # And convert camel case attributes to snake case one.
+    #
+    # @param param [String] format string. e.g. `is:open is:close senro gem`
+    # @return [String] formated stirng
+    def self.query(param)
+      data = { query: ''.dup, status: {} }
+      elements = param.split(' ')
+      elements.each_with_object(data) do |ele, h|
+        if ele.include? ':'
+          ary = ele.split(':')
+          h[:status][ary[0].underscore.to_sym] =
+            Array(h[:status][ary[0].underscore.to_sym]) << ary[1]
+        else
+          h[:query] << (h[:query] == '' ? ele : " #{ele}")
+        end
+      end
+    end
   end
 end

eb9db941c8df4ed1340f36fce9fa642e7af8fbcf
WalkerSumida <walkersumida@gmail.com>
2020-04-16 13:58:47 +0900

    test: change return values from string to hash

--- spec/senro/query_params_formatter/sort_spec.rb
+++ spec/senro/query_params_formatter/sort_spec.rb
@@ -4,19 +4,19 @@ RSpec.describe Senro::QueryParamsFormatter do
   describe '#sort' do
     context 'with +' do
       it 'returns order clause containing ASC' do
-        expect(Senro::QueryParamsFormatter.sort('+id')).to eq('id ASC')
+        expect(Senro::QueryParamsFormatter.sort('+id')).to eq({ id: 'asc' })
       end
     end
 
     context 'with -' do
       it 'returns order clause containing DESC' do
-        expect(Senro::QueryParamsFormatter.sort('-id')).to eq('id DESC')
+        expect(Senro::QueryParamsFormatter.sort('-id')).to eq({ id: 'desc' })
       end
     end
 
     context 'without + and -' do
       it 'returns order clause containing ASC' do
-        expect(Senro::QueryParamsFormatter.sort('id')).to eq('id ASC')
+        expect(Senro::QueryParamsFormatter.sort('id')).to eq({ id: 'asc' })
       end
     end
 
@@ -24,7 +24,7 @@ RSpec.describe Senro::QueryParamsFormatter do
       it 'returns correct order clause' do
         expect(
           Senro::QueryParamsFormatter.sort('+id,+name,created_at,-updated_at')
-        ).to eq('id ASC, name ASC, created_at ASC, updated_at DESC')
+        ).to eq({ id: 'asc', name: 'asc', created_at: 'asc', updated_at: 'desc' })
       end
     end
 
@@ -32,7 +32,7 @@ RSpec.describe Senro::QueryParamsFormatter do
       it 'returns snake case attributes' do
         expect(
           Senro::QueryParamsFormatter.sort('+id,+name,createdAt,-updatedAt')
-        ).to eq('id ASC, name ASC, created_at ASC, updated_at DESC')
+        ).to eq({ id: 'asc', name: 'asc', created_at: 'asc', updated_at: 'desc' })
       end
     end
   end
